5d5d2ae465929abf2cb5abbb85c3c31103b435c3
Toni Tiilikainen <ototi009@edu.xamk.fi>
2023-11-19 20:30:29 +0200

    State with enums

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,24 @@ public class Move : MonoBehaviour
     {
         JUMPING,
         STANDING,
-        CROUCHING,
-        REPLAYING
+        CROUCHING
     }
 
-    [SerializeField]
-    private PlayerState my_state = PlayerState.JUMPING;
+    enum ColorState
+    {
+        NORMAL,
+        GREEDY,
+        KILLER
+    }
 
+    [SerializeField] private bool grounded = false;
+
+    [SerializeField] private float colorTimer;
+    
+    [SerializeField] private PlayerState my_state = PlayerState.JUMPING;
+    [SerializeField] private ColorState my_color = ColorState.NORMAL;
+    [SerializeField] private Renderer _renderer;
+    
     public float jumpForce = 10;
 
     [SerializeField] private Rigidbody rb;
@@ -33,39 +45,110 @@ public class Move : MonoBehaviour
     private float replayInterval = .1f;
     private float timer = 0;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnEnable()
     {
-        
+        Coin.OnCoinCollected += GotGreedy;;
+        Enemy.OnEnemyDestroyed += GotBloody;
     }
 
+    private void OnDisable()
+    {
+        Coin.OnCoinCollected -= GotGreedy;;
+        Enemy.OnEnemyDestroyed -= GotBloody;
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(new Vector3(0,1,0) * jumpForce, ForceMode.Impulse);
+    }
+
+    private void GotGreedy()
+    {
+        colorTimer = 3;
+        _renderer.material.color = Color.yellow;
+        my_color = ColorState.GREEDY;
+    }
+
+    private void GotBloody()
+    {
+        colorTimer = 3;
+        _renderer.material.color = Color.red;
+        my_color = ColorState.KILLER;
+    }
 
     void Update()
     {
         if (!replaying)
         {
-            // jump
-            if (Input.GetKeyDown(KeyCode.Space))
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + new Vector3(0, .9f, 0),  Vector3.down, out hit, 1f))
             {
-                rb.AddForce(new Vector3(0,1,0) * jumpForce, ForceMode.Impulse);
+                grounded = true;
             }
-
-            // crouch
-            if (Input.GetKeyDown(KeyCode.LeftControl))
+            else
             {
-
+                grounded = false;
             }
             
             switch (my_state)
             {
                 case PlayerState.JUMPING:
+                    if (grounded)
+                    {
+                        my_state = PlayerState.STANDING;
+                    }
                     break;
                 case PlayerState.STANDING:
+                    if (!grounded)
+                    {
+                        my_state = PlayerState.JUMPING;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        Jump();
+                        my_state = PlayerState.JUMPING;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.LeftControl))
+                    {
+                        this.transform.localScale = new Vector3(1, .7f, 1);
+                        my_state = PlayerState.CROUCHING;
+                    }
                     break;
                 case PlayerState.CROUCHING:
+                    if (!grounded)
+                    {
+                        my_state = PlayerState.JUMPING;
+                    }
+                    else if (Input.GetKeyUp(KeyCode.LeftControl))
+                    {
+                        this.transform.localScale = new Vector3(1, 1, 1);
+                        my_state = PlayerState.STANDING;
+                    }
                     break;
             }
-            
 
+            switch (my_color)
+            {
+                case ColorState.NORMAL:
+                    _renderer.material.color = Color.blue;
+                    break;
+                case ColorState.GREEDY:
+                    colorTimer -= Time.deltaTime;
+                    if (colorTimer < 0)
+                    {
+                        colorTimer = 0;
+                        my_color = ColorState.NORMAL;
+                    }
+                    break;
+                case ColorState.KILLER:
+                    colorTimer -= Time.deltaTime;
+                    if (colorTimer < 0)
+                    {
+                        colorTimer = 0;
+                        my_color = ColorState.NORMAL;
+                    }
+                    break;
+            }
 
             if (Input.GetKeyDown(KeyCode.W))
             {
